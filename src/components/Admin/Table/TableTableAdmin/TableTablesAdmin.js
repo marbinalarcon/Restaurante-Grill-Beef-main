259361d2ed99a4eb6c660d92015099a42e6877bb
CarlosLopez03 <carloseli0297@gmail.com>
2022-01-23 19:41:09 -0500

    Trabajo finalizado

--- src/components/Admin/Table/TableTableAdmin/TableTablesAdmin.js
+++ src/components/Admin/Table/TableTableAdmin/TableTablesAdmin.js
@@ -1,41 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import { Table, Button, Icon } from "semantic-ui-react";
 import { map } from "lodash";
 import "./TableTablesAdmin.scss";
+import { ModalBasic } from "../../../Common";
+import QRCode from "qrcode.react";
 
 export function TableTablesAdmin(props) {
   const { tables, updateTable, deleteTable } = props;
+  const [showModal, setShowModal] = useState(false);
+  const [contentModal, setContentModal] = useState(null);
+
+  const openCloseModal = () => setShowModal((prev) => !prev);
+
+  const showQr = (table) => {
+    setContentModal(
+      <div style={{ textAlign: "center" }}>
+        <QRCode value={`${window.location.origin}/client/${table.number}`} />
+      </div>
+    );
+    openCloseModal();
+  };
 
   return (
-    <Table className="table-tables-admin">
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell>Numero</Table.HeaderCell>
-          <Table.HeaderCell></Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
-
-      <Table.Body>
-        {map(tables, (table, index) => (
-          <Table.Row key={index}>
-            <Table.Cell> {table.number}</Table.Cell>
-            <Actions
-              table={table}
-              updateTable={updateTable}
-              deleteTable={deleteTable}
-            />
+    <>
+      <Table className="table-tables-admin">
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Numero</Table.HeaderCell>
+            <Table.HeaderCell></Table.HeaderCell>
           </Table.Row>
-        ))}
-      </Table.Body>
-    </Table>
+        </Table.Header>
+
+        <Table.Body>
+          {map(tables, (table, index) => (
+            <Table.Row key={index}>
+              <Table.Cell> {table.number}</Table.Cell>
+              <Actions
+                table={table}
+                updateTable={updateTable}
+                deleteTable={deleteTable}
+                showQr={showQr}
+              />
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+
+      <ModalBasic
+        show={showModal}
+        onClose={openCloseModal}
+        title="Codigo QR"
+        size="mini"
+        children={contentModal}
+      />
+    </>
   );
 }
 
 function Actions(props) {
-  const { table, updateTable, deleteTable } = props;
+  const { table, updateTable, deleteTable, showQr } = props;
   return (
     <Table.Cell textAlign="right">
-      <Button icon onClick={() => console.log(table)}>
+      <Button icon onClick={() => showQr(table)}>
         <Icon name="qrcode" />
       </Button>
       <Button icon onClick={() => updateTable(table)}>
